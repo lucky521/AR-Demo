6b8b8952c2906e6b37884fad9ff7151248d7462e
Lucky <307479836@qq.com>
2017-02-28 14:24:43 +0800

    First version

---
+++ Assets/lu_ball.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class lu_ball : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if(this.transform.position.y < -5)
+		{
+			this.transform.position = new Vector3 (0, 5, 2);
+		}
+
+	}
+}
---
+++ Assets/lu_plane.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class lu_plane : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+	}
+}
